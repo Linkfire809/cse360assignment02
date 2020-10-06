b91cc02cc4c65882f7c366aaad78af7b46f4bcfa
Andrew Tran <atran32@asu.edu>
2020-10-05 20:36:40 -0700

    Creation of AddingMachine file with Javadoc comments

---
+++ cse360assignment02/src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,62 @@
+package cse360assignment02;
+
+/**
+ * The AddingMachine program is an application designed
+ * as a simple calculator program that adds or subtracts values
+ * then displays the total.
+ * 
+ * @author Andrew Tran
+ * @version 1.0
+ * @since 2020-10-05
+ *
+ */
+public class AddingMachine {
+	private int total;
+	
+	/**
+	 * AddingMachine class constructor.
+	 * 
+	 */
+	public AddingMachine() {
+		total = 0; //not needed - included for clarity
+	}
+	
+	/**
+	 * Method getTotal returns the total value from calculations
+	 * @return 0
+	 */
+	public int getTotal() {
+		return 0;
+	}
+	
+	/**
+	 * Method add returns the value made from adding the given int value to the total.
+	 * @param value
+	 */
+	public void add(int value) {
+		
+	}
+	
+	/**
+	 * Method subtract returns the value from subtracting the given int value from the total.
+	 * @param value
+	 */
+	public void subtract(int value) {
+		
+	}
+	
+	/**
+	 * Method toString converts what is implemented to a string.
+	 */
+	public String toString() {
+		return "";
+	}
+	
+	/**
+	 * Method clear wipes the total recording from toString.
+	 */
+	public void clear() {
+		
+	}
+	
+}
